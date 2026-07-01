02017f42691037f216ddb46ae816dfcaafa3ae9b
Nguyen Minh Tuan <nguyenminhtuan251006@gmail.com>
2026-07-01 11:15:23 +0700

    Update __init__.py

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,9 @@
+"""Package initializer for the EmotionDetection application module.
+
+This file imports the emotion_detector function from the
+emotion_detection module so it can be used by the application.
+"""
+
 from .emotion_detection import emotion_detector
+
+__all__ = ["emotion_detector"]
